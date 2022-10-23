c26a51f7468754fbe41e528db13c68e93385844b
hashan nalaka <hashannalaka0123@gmail.com>
2022-10-23 18:08:18 +0530

    final commit
    IN-02

---
+++ basics/asynchronous.js
@@ -0,0 +1,13 @@
+
+console.log(" I ");
+
+console.log(" eat ");
+
+setTimeout( ()=>{
+    console.log("ice cream")
+}, 4000 )
+
+console.log(" spoon ");
+
+console.log(" with ");
+
---
+++ basics/synchronous.js
@@ -0,0 +1,11 @@
+
+console.log(" I ");
+
+console.log(" eat ");
+
+console.log(" spoon ");
+
+console.log(" ice cream ");
+
+console.log(" with ");
+
